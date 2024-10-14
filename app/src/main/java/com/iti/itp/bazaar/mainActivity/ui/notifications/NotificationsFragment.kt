5d8a549161273fcf40e994243ac116f11283eaac
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-14 10:50:18 +0300

    Added Design on Me Screen

--- app/src/main/java/com/iti/itp/bazaar/mainActivity/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/iti/itp/bazaar/mainActivity/ui/notifications/NotificationsFragment.kt
@@ -11,32 +11,20 @@ import com.iti.itp.bazaar.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var binding:FragmentNotificationsBinding
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
