ac77134dc47099d553c1cb1cc58001b28994a3c0
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-17 00:16:29 +0300

    fixing navigate between activities and fragments

--- app/src/main/java/com/iti/itp/bazaar/settings/SettingsActivity.kt
+++ app/src/main/java/com/iti/itp/bazaar/settings/SettingsActivity.kt
@@ -1,20 +1,36 @@
 package com.iti.itp.bazaar.settings
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.iti.itp.bazaar.R
-import com.iti.itp.bazaar.databinding.FragmentSettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        findNavController(R.id.nav_host_fragment_activity_settings)
+        // Initialize NavController
+        navController = findNavController(R.id.nav_host_fragment_activity_settings)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (navController.currentDestination?.id == navController.graph.startDestinationId) {
+            // If we're at the start destination, finish the activity
+            super.onBackPressed()
+        } else {
+            // Otherwise navigate up within the settings navigation graph
+            navController.navigateUp()
+        }
     }
 }
